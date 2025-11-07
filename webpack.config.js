f55ae10a2d09b49fa3033d9ca235aa563eaa3789
zaga9436 <zaga9436@gmail.com>
2025-11-07 20:50:54 +0200

    добавил обработку изображений

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         test: /\.css$/i, 
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, 
+        type: 'asset/resource',          
+      },
     ],
   },
 };
