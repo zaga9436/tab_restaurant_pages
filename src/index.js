8a4f66efabe4d58a3d2dbe88b059fa0a1ae1355e
zaga9436 <zaga9436@gmail.com>
2025-11-07 20:50:12 +0200

    добавил переключение к menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import loadHome from './home.js';
+import loadMenu from './menu.js';
 
 
 const content = document.getElementById('content');
+const menuBtn = document.getElementById('menu');
+
+function clearContent() {
+  content.textContent = '';
+}
+
+menuBtn.addEventListener('click', () => {
+  clearContent();
+  loadMenu(content);
+});
+
+
+const homeBtn = document.getElementById('home');
+homeBtn.addEventListener('click', () => {
+  clearContent();
+  loadHome(content);
+});
+
+
+
 
 loadHome(content);
