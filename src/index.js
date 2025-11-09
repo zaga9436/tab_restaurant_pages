e1c3466bfa232c5ac66e63c59972fed6ae67db48
zaga9436 <zaga9436@gmail.com>
2025-11-09 15:47:54 +0200

    index.js ready

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
-import loadHome from './home.js';
-import loadMenu from './menu.js';
+import "./style.css";
+import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContactPage from "./contact";
 
+console.log("We are serving");
 
-const content = document.getElementById('content');
-const menuBtn = document.getElementById('menu');
+const btns = document.querySelectorAll("button");
 
-function clearContent() {
-  content.textContent = '';
+const active = document.querySelector(".active");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = function () {
+    let move = (100 / btns.length) * i;
+    active.style.left = move + "%";
+  };
 }
 
-menuBtn.addEventListener('click', () => {
-  clearContent();
-  loadMenu(content);
+const content = document.querySelector("#content");
+
+function cleanContent() {
+  content.innerHTML = "";
+}
+
+const html = document.body;
+
+function clearBackgroundClasses(element){
+  element.classList.remove("homepage-img");
+}
+const tabs = document.querySelector(".tabs");
+
+tabs.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "home":
+      cleanContent();
+      clearBackgroundClasses(html);
+      html.classList.add('homepage-img');
+      loadHome(content);
+      break;
+    case "menu":
+      cleanContent();
+      clearBackgroundClasses(html);
+      loadMenu(content);
+      break;
+    case "contact":
+      cleanContent();
+      clearBackgroundClasses(html);
+      loadContactPage(content);
+      break;
+  }
 });
-
-
-const homeBtn = document.getElementById('home');
-homeBtn.addEventListener('click', () => {
-  clearContent();
-  loadHome(content);
-});
-
-
-
 
 loadHome(content);
